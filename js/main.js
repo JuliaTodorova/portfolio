ee451fa438d6a231d99850bef67088e231339aa8
Julia Todorova <juliastoycheva@gmail.com>
2014-05-23 10:02:11 -0700

    start of portfolio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Scroll Navigation
+
+$(document).ready(function() 
+{
+	$(".scroll").on("click",function(e){
+
+		e.preventDefault();
+
+		$("body, html").animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 900);
+
+	});
+});
+
+// scroll-to-top button show and hide
+
+jQuery(document).ready(function(){
+    jQuery(window).scroll(function(){
+        if (jQuery(this).scrollTop() > 100) {
+            jQuery('.scrollup').fadeIn();
+        } else {
+            jQuery('.scrollup').fadeOut();
+    }
+});
+// scroll-to-top animate
+jQuery('.scrollup').click(function(){
+    jQuery("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
+
+// Landing page preloader
